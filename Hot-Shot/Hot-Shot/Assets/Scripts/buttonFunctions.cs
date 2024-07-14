f0d61e46e6c3e8aacecbac79a12a0eff1edcea32
Chanel Maughmer <146034501+r3dd89@users.noreply.github.com>
2024-07-13 19:10:11 -0500

    Made changes to enemy AI.
    Enemy now shoots at the player and looks in the player's direction.....still can't get the enemy to actually move towards the player.

--- Hot-Shot/Hot-Shot/Assets/Scripts/buttonFunctions.cs
+++ Hot-Shot/Hot-Shot/Assets/Scripts/buttonFunctions.cs
@@ -6,36 +6,44 @@ using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
+    // Resumes the game by unpausing it
     public void resume()
     {
         gameManager.instance.stateUnpause();
     }
 
+    // Restarts the game by reloading the current scene
     public void restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         gameManager.instance.stateUnpause();
     }
 
+    // Opens the settings menu
     public void settings()
     {
         gameManager.instance.OpenSettingsMenu();
     }
+
+    // Goes back to the previous menu
     public void back()
     {
         gameManager.instance.back();
     }
+
+    // Toggles the Y-axis inversion setting
     public void onToggleChange()
     {
         gameManager.instance.toggleInvertY();
     }
 
+    // Quits the game or stops play mode in the Unity Editor
     public void quit()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-                Application.Quit();
+        Application.Quit();
 #endif
     }
 }
--- Hot-Shot/Hot-Shot/Assets/Scripts/gameManager.cs
+++ Hot-Shot/Hot-Shot/Assets/Scripts/gameManager.cs
@@ -7,80 +7,88 @@ using UnityEngine.UI; //Add using UnityEngine.UI
 
 public class gameManager : MonoBehaviour
 {
-    public static gameManager instance;
+    public static gameManager instance; // Singleton instance
 
-    [SerializeField] GameObject menuActive;
-    [SerializeField] GameObject menuPause;
-    [SerializeField] GameObject menuWin;
-    [SerializeField] GameObject menuLose;
-    [SerializeField] GameObject menuSettings;
-    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] GameObject menuActive; // The currently active menu
+    [SerializeField] GameObject menuPause; // The pause menu
+    [SerializeField] GameObject menuWin; // The win menu
+    [SerializeField] GameObject menuLose; // The lose menu
+    [SerializeField] GameObject menuSettings; // The settings menu
 
-   
-    public GameObject player;
-    public PlayerMovement playerScript;
-    public PlayerVision playerVisionScript;
-    public GameObject prevMenu;
-    public Image playerHPBar; //Variable for player HP bar
-    public GameObject damageFlashScreen; //Variable for damage flash
+    public GameObject player; // The player GameObject
+    public PlayerMovement playerScript; // Reference to the PlayerMovement script
+    public PlayerVision playerVisionScript; // Reference to the PlayerVision script
+    public GameObject prevMenu; // The previous menu before the current one
+    public GameObject damageFlashScreen; // The screen that flashes when the player takes damage
 
-    public bool isPaused;
-    public bool invertY;
+    public bool isPaused; // Whether the game is currently paused
+    public bool invertY; // Whether the Y-axis is inverted
 
-    int enemyCount;
+    int enemyCount; // The number of enemies in the game
 
-    // Start is called before the first frame update
-    //order of openning, awake > start > other
-    //awake for managers
+    // Awake is called before Start and is used to initialize any variables or game state before the game starts
     void Awake()
     {
+        // Set the singleton instance to this object
         instance = this;
+
+        // Find the player GameObject and get its PlayerMovement script
         player = GameObject.FindWithTag("Player");
         playerScript = player.GetComponent<PlayerMovement>();
+        playerVisionScript = player.GetComponent<PlayerVision>(); // Initialize playerVisionScript
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check if the Cancel button (usually Esc) is pressed
         if (Input.GetButtonDown("Cancel"))
+        {
+            // If no menu is active, pause the game and show the pause menu
             if (menuActive == null)
             {
                 statePause();
                 menuActive = menuPause;
                 menuActive.SetActive(isPaused);
             }
+            // If the pause menu is active, unpause the game
             else if (menuActive == menuPause)
+            {
                 stateUnpause();
-
-    }
-    public void statePause()
-    {
-
-        isPaused = !isPaused;
-        Time.timeScale = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.Confined;
-
-    }
-
-    public void stateUnpause()
-    {
-
-        isPaused = false;
-        Time.timeScale = 1;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
-        if (menuActive != null)
-        {
-            menuActive.SetActive(false);
-            menuActive = null;
+            }
         }
     }
 
+    // Pauses the game
+    public void statePause()
+    {
+        isPaused = !isPaused; // Toggle the paused state
+        Time.timeScale = 0; // Stop the game time
+        Cursor.visible = true; // Show the cursor
+        Cursor.lockState = CursorLockMode.Confined; // Confine the cursor to the game window
+    }
+
+    // Unpauses the game
+    public void stateUnpause()
+    {
+        isPaused = false; // Set paused state to false
+        Time.timeScale = 1; // Resume the game time
+        Cursor.visible = false; // Hide the cursor
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+
+        if (menuActive != null)
+        {
+            menuActive.SetActive(false); // Hide the active menu
+            menuActive = null; // Clear the active menu reference
+        }
+    }
+
+    // Updates the game goal by adjusting the enemy count
     public void updateGameGoal(int amount)
     {
-        enemyCount += amount;
+        enemyCount += amount; // Adjust the enemy count
+
+        // If all enemies are defeated, show the win menu
         if (enemyCount <= 0)
         {
             statePause();
@@ -89,15 +97,14 @@ public class gameManager : MonoBehaviour
         }
     }
 
+    // Shows the win game menu and pauses the game
     public void WinGame()
     {
         menuWin.SetActive(true);
-
-        // Pauses the game
-        //https://gamedevbeginner.com/the-right-way-to-pause-the-game-in-unity/
-        Time.timeScale = 0;
+        Time.timeScale = 0; // Pause the game time
     }
 
+    // Shows the lose game menu and pauses the game
     public void LoseGame()
     {
         statePause();
@@ -105,46 +112,53 @@ public class gameManager : MonoBehaviour
         menuActive.SetActive(isPaused);
     }
 
+    // Restarts the game by reloading the current scene
     public void RestartGame()
     {
-        Time.timeScale = 0;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 0; // Pause the game time
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload the current scene
     }
+
+    // Opens the settings menu
     public void OpenSettingsMenu()
     {
-        if(menuActive != null)
-    {
-            prevMenu = menuActive;
-            menuActive.SetActive(false);
+        if (menuActive != null)
+        {
+            prevMenu = menuActive; // Save the current active menu
+            menuActive.SetActive(false); // Hide the current active menu
         }
 
-        menuSettings.SetActive(true);
-        menuActive = menuSettings;
-        statePause();
+        menuSettings.SetActive(true); // Show the settings menu
+        menuActive = menuSettings; // Set the settings menu as the active menu
+        statePause(); // Pause the game
     }
+
+    // Goes back to the previous menu
     public void back()
     {
         Debug.Log("Back pressed");
         if (menuActive != null)
         {
             Debug.Log("Hiding current active menu");
-            menuActive.SetActive(false);
+            menuActive.SetActive(false); // Hide the current active menu
         }
 
         if (prevMenu != null)
         {
             Debug.Log("showing previous menu");
-            prevMenu.SetActive(true);
-            menuActive = prevMenu;
-            prevMenu = null;
+            prevMenu.SetActive(true); // Show the previous menu
+            menuActive = prevMenu; // Set the previous menu as the active menu
+            prevMenu = null; // Clear the previous menu reference
         }
 
-        statePause();
+        statePause(); // Pause the game
     }
+
+    // Toggles the Y-axis inversion
     public void toggleInvertY()
     {
-        invertY = !invertY;
-        if (playerVisionScript != null) 
-            playerVisionScript.invertY = invertY;
+        invertY = !invertY; // Toggle the invertY variable
+        if (playerVisionScript != null)
+            playerVisionScript.invertY = invertY; // Update the PlayerVision script
     }
 }
